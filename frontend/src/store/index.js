e4444e6f1eaaaac3568b00bf77e7a6b46dec0ff6
Tobias Klaus <tk@nuiteq.com>
2020-03-09 21:50:36 +0100

    save jwt in localstorage and fetch on state init

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -6,13 +6,14 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
 	state: {
-		idToken: null,
+		idToken: getSavedState('auth.idToken'),
 		userId: null
 	},
 	mutations: {
 		authUser(state, userData) {
 			state.idToken = userData;
 			axios.defaults.headers.common['Authorization'] = 'jwt ' + userData;
+			saveState('auth.idToken', userData);
 		}
 	},
 	actions: {
@@ -22,3 +23,11 @@ export default new Vuex.Store({
 	},
 	modules: {}
 });
+
+function getSavedState(key) {
+	return JSON.parse(window.localStorage.getItem(key));
+}
+
+function saveState(key, JWT) {
+	window.localStorage.setItem(key, JSON.stringify(JWT));
+}
